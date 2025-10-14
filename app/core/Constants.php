523bd9c17ffcc3be1dbb84145b2ef27964d86332
KikyAja31 <2411500032@mahasiswa.atmaluhur.ac.id>
2025-10-14 08:20:01 +0700

    add: Any File

---
+++ app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php
+
+define('BASEURL', 'http://localhost/PWBO-RIFQIALGHIFARI/public');
---
+++ app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container">
+    <h1>About Me</h1>
+    <img src="<?php echo BASEURL; ?>/img/bocchi.jpg" width="200" class="rounded-circle shadow">
+    <p>Halo, Nama saya <?php echo $data['nama']; ?>, saya adalah seorang <?php echo $data['pekerjaan']; ?></p>
+</div>
