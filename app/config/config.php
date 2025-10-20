bd8150419c5c70b561cc2c8ac77c0573df4931b8
KikyAja31 <2411500032@mahasiswa.atmaluhur.ac.id>
2025-10-21 02:56:31 +0700

    Add & Update Files: Last Page 87

---
+++ app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('BASEURL', 'http://localhost/PWBO-RIFQIALGHIFARI/public');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'pwbo_2411500032');
--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,15 +1,57 @@
 <div class="container mt-4">
     <div class="row">
         <div class="col-6">
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">Tambah Data Mahasiswa</button>
             <h3>Daftar Mahasiswa</h3>
-            <?php foreach($data['mhs'] as $mhs) : ?>
-                <ul>
-                    <li><?php echo $mhs['nama']; ?></li>
-                    <li><?php echo $mhs['nim']; ?></li>
-                    <li><?php echo $mhs['email']; ?></li>
-                    <li><?php echo $mhs['jurusan']; ?></li>
-                </ul>
-            <?php endforeach; ?>
+            <ul class="list-group">
+                <?php foreach( $data['mhs'] as $mhs ) : ?>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        <?= $mhs['nama']; ?>
+                        <a href="<?php echo BASEURL; ?>/mahasiswa/detail/<?php echo $mhs['id']; ?>" class="badge badge-primary">Detail</a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
         </div>
     </div>
+</div>
+
+<div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="judulModal" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="POST">
+          <div class="form-group">
+            <label for="nama">Nama</label>
+            <input type="text" class="form-control" id="nama" name="nama">
+        </div>
+        <div class="form-group">
+            <label for="nim">NIM</label>
+            <input type="text" class="form-control" id="nim" name="nim">
+        </div>
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" class="form-control" id="email" name="email">
+        </div>
+        <div class="form-group">
+            <label for="jurusan">Jurusan</label>
+            <select class="form-control" name="jurusan" id="jurusan">
+                <option value="Informatika">Informatika</option>
+                <option value="Sistem Informasi">Sistem Informasi</option>
+                <option value="Teknik Komputer">Teknik Komputer</option>
+                <option value="Manajemen Informatika">Manajemen Informatika</option>
+            </select>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary">Tambah Data</button>
+        </form>
+      </div>
+    </div>
+  </div>
 </div>
